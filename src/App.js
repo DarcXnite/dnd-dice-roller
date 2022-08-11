c61062622796a0d52ade09a7f0227305f24b363d
Davis Nguyen <davisnguyen@Daviss-MacBook-Air.local>
2022-08-11 15:12:00 -0500

    finished rng function with die count

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import D4 from './components/d4'
 
 function App() {
   const [d4Count, setD4Count] = useState(3)
+  const [d6Count, setD6Count] = useState(3)
+  const [d8Count, setD8Count] = useState(3)
+  const [d10Count, setD10Count] = useState(3)
+  const [d12Count, setD12Count] = useState(3)
+  const [d20Count, setD20Count] = useState(3)
+  const [d100Count, setD100Count] = useState(3)
 
   const rng = sides => {
     return Math.floor(Math.random() * sides) + 1
@@ -15,31 +21,37 @@ function App() {
   }
 
   const d6 = () => {
-    for (let i = 1; i <= d4Count; i++) {
+    for (let i = 1; i <= d6Count; i++) {
       console.log(rng(6))
     }
   }
 
+  const d8 = () => {
+    for (let i = 1; i <= d8Count; i++) {
+      console.log(rng(8))
+    }
+  }
+
   const d10 = () => {
-    for (let i = 1; i <= d4Count; i++) {
+    for (let i = 1; i <= d10Count; i++) {
       console.log(rng(10))
     }
   }
 
   const d12 = () => {
-    for (let i = 1; i <= d4Count; i++) {
+    for (let i = 1; i <= d12Count; i++) {
       console.log(rng(12))
     }
   }
 
   const d20 = () => {
-    for (let i = 1; i <= d4Count; i++) {
+    for (let i = 1; i <= d20Count; i++) {
       console.log(rng(20))
     }
   }
 
   const d100 = () => {
-    for (let i = 1; i <= d4Count; i++) {
+    for (let i = 1; i <= d100Count; i++) {
       console.log(rng(100))
     }
   }
