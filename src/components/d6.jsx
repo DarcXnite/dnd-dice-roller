5ff5327b445e1f07c92731b6d5a78418ff3a083f
Emily-Herndon <emilyvherndon@gmail.com>
2022-08-11 13:14:21 -0700

    added framework to dice components

---
+++ src/components/d6.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function d6() {
+    return (
+        <div>d6</div>
+    )
+}
---
+++ src/components/d8.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function d8() {
+    return (
+        <div>d8</div>
+    )
+}
